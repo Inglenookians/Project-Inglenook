4ab23f760200e4f9495592c873288e57ffc3b3f6
glim <glim@developers.project-inglenook.co.uk>
2012-10-30 01:21:36 +0000

    Several changes, fixes, and enhancements: MERGE: pulled master which now contains initial versions of core, locate, settings and directories. MODIFIED: directory structure and naming to conform to cmake system              logging library moved to ./src/lib/ign_logging              example code to ./src/examples/lib/ign_logging MODIFIED: Sample code from one main.cpp I was using for testing to a proper tutorial-esqe file (01-basic.cpp) and a threading test/demo(02-threading.cpp). MODIFIED: all code made to build with new cmake system. MODIFIED: cmake system to accept -DWITH_EXAMPLES=[ON|OFF] switch (ON by default) which will indicate whether the examples directory should build or not. MODIFIED: setup-builds.sh to pass arguments though to cmake (so I can actually use my -DWITH_EXAMPLES switch)               - don't move the insertion point for arguments else you might bork the build system (if you override the build type). MODIFIED: log_writer to use core's implamentation of pid and process name lookup methods. FIXED: use of std::manipulators broke log_client (such as std::endl, std::end, etc), a new overload has been provided to handle this.

---
+++ src/examples/lib/ign_logging/01-basic.cpp
@@ -0,0 +1,128 @@
+/*
+ * main.cpp: Main software entry point for logging demonstrator.
+ * Copyright (C) 2012, Project Inglenook (http://www.project-inglenook.co.uk)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// inglenook includes
+#include <ign_logging/logging.h>	// this header is required for all logging.
+
+/**
+ * Main software entry point
+ * This program demonstrates on the most simple level, how to use the logging library,
+ * note that code in this sample does not conform to usual release standards as it has
+ * been simplified to make the sample as clear as possible.
+ * @param arg_c number of command line arguments.
+ * @param arg_v character array delimited software arguments
+ */
+int main(int arg_c, char* arg_v[])
+{
+
+	// this simplifies accessing the logging interface (ilog).
+	using namespace inglenook::logging;
+	using namespace inglenook;
+
+	// the easiest way to use the logging system is to use the stream operators.
+	ilog.info() << "Hello World" << lf::end;
+
+	// note in the above line we are writing to the standard information log. in addition,
+	// you can write to the follow other log types
+	ilog.debug() 	<< "Debugging messages should be written to debug()" 		<< lf::end;
+	ilog.trace() 	<< "Verbose messages should be written to trace()" 			<< lf::end;
+	ilog.warning() 	<< "Warning messages should be written to warning()" 		<< lf::end;
+	ilog.error() 	<< "Non-fatal error messages should be written to error()" 	<< lf::end;
+	ilog.fatal()	<< "Fatal error messages should be written to fatal()" 		<< lf::end;
+
+	// IMPORTANT: be aware that the logs are filtered, as such despite all the above messages
+	//   being sent to the log manager, they may not be actually written to disk, or displayed
+	//   on the screen. The thresholds at which a log is written to disk, or printed to the
+	//   screen can be set independently.
+
+	// All messages are finished with an lf::end, this means you can break entries up over lines.
+	// (note: this is thread safe, no additional thread safety work is required on your behalf).
+	int a = 1, b = 2;
+	ilog.info() << "The value of a + b is ";
+	int c = a + b;
+	ilog << c << lf::end;
+
+	// Note: when we write the answer to the log (the c variable), because the category is already
+	//    set to info() we do not need to specify this again until we start the next message.
+
+	// IMPORTANT: technically no category needs to be assigned until the message is "flushed" with
+	//    a lf::end stream manipulator. It is however a strongly recommended inglenook convention
+	//    however to always start a log entry with the category set as it make the log easier to read.
+	//    Failure to set the namespace at all before sending a lf::end, will cause the message to be
+	//    rejected.
+
+	// you can also use this behaviour to change the log category, the following message will be
+	// written to the log as an error entry because the last write was to the error category.
+	ilog.info() << "Attempting to remove cute from Kittens: ";
+	ilog.error() << "Don't be ridiculous" << lf::end;
+
+	// up until now we've been naughty. every message we have written has not been namespaced.
+	// inglenook groups messages in namespaces to help group associated logs. We can specify the
+	// namespace of an log entry with the ns() stream manipulator. The following example writes
+	// to the examples.logging.01 namespace
+	ilog.info() << ns("examples.logging.01") << "A well mannered \"Hello World\"" << lf::end;
+
+	// obviously, this will get tedious if we do this on every message, most functionality should
+	// be grouped within a given context. As such we can set a default namespace with:
+	ilog.default_namespace("examples.logging.01.default");
+
+	// Note: the ilog (log_client) default namespace is thread specific (and as such thread safe).
+	//    It has to be set per thread. If you do not do this, messages are written to the log_output
+	//    (log_writer) default namespace. By default log_output's default namespace is
+	//    "inglenook.anonymous". Because of this it is often more convient to reset the writers default
+	//    namespace instead, this can be done as follows:
+	log_output->default_namespace("examples.logging.01");
+
+	// IMPORTANT: the default namespace on the log_writer is NOT thread safe. It should be set once,
+	//    immediately when the program is started. Modifification of this variable after this point
+	//    is strongly discouraged (I know, I have here), as in a multithreaded environment, the
+	//    result of reassigning this variable is undefined.
+
+	// IMPORTANT: namespaces are NOT sanitized when written to disk. As such they should never be
+	//    used in such a way in which user input is able to dictate the namespace text. This could
+	//    be used to compromise the systems security.
+
+	// finally, sometimes it is nice to attach a little more context to a log. To support this we have
+	// the log_data() stream manipulator.
+	ilog.warning() << "This is a 'levels of something' warning!"
+				   << log_data("level of something", "17,000")
+				   << log_data("acceptable level",   "15,000")
+				   << lf::end;
+
+	// Note: data and values can only be strings currently. Sorry, you'll have to stringify anything you need
+	//      yourself.
+
+	// IMPORTANT: the log_data key (first parameter) is NOT sanitized when written to disk. As such it
+	//    should never be used in such a way in which user input is able to dictate the key text. This could
+	//    be used to compromise the systems security.
+
+	// finally; if for some reason you wish to manually build a log entry. you can, this is how:
+	auto le = std::shared_ptr<log_entry>(new log_entry());
+	le->message("This is a hand crafted, finely made log message.");
+	le->log_namespace("examples.logging.01");
+	le->extended_data("Hand made", "You bet");
+	le->extended_data("Why", "Who knows");
+	le->entry_type(category::information);
+	log_output->add_entry(le);
+
+	// this ends the quick introduction to the basics of logging.
+	// if you run this; you might want to delete the log file
+	// hint: /var/log/inglenook/[app-name]/
+	//
+
+}
---
+++ src/examples/lib/ign_logging/02-threading.cpp
@@ -0,0 +1,116 @@
+/*
+ * main.cpp: Main software entry point for logging demonstrator.
+ * Copyright (C) 2012, Project Inglenook (http://www.project-inglenook.co.uk)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// standard library includes
+#include <stdlib.h>
+#include <string>
+#include <sstream>
+
+// boost (http://boost.org) includes
+#include <boost/exception/all.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/posix_time/posix_time_io.hpp>
+
+// inglenook includes
+#include <ign_logging/logging.h>
+
+/**
+ * Log testing thread.
+ * This thread is designed to test the capabilities of the log_client object under load and demonstrate the
+ * simplicity of using the library with threads (no work required!)
+ * @param id thread id (index) within starting array.
+ * @param no_outputs character array delimited software arguments
+ */
+void loud_thread(int id, int no_outputs)
+{
+	// this simplifies accessing the logging interface (iout).
+	using namespace inglenook::logging;
+	using namespace inglenook;
+
+	// change the thread specific default category to warning.
+	ilog.default_entry_type(category::warning);
+
+	// notify caller that we have started.
+	ilog.info() << "thread #" << id << " started." << lf::end;
+
+	// spam a few messages out to the log.
+	for(int i = 0; i < no_outputs; i++)
+	{
+		// write the message, lots of places for bad thread safety to show...
+		ilog << "this is message #" << (i+1) << " of " <<
+				no_outputs << " from thread " << id << lf::end;
+	}
+
+	// notify thread complete
+	ilog.info() << "thread #" << id
+			<< " has completed!" << lf::end;
+
+}
+
+/**
+ * Logging daemon software entry point.
+ * Execution starts at this method. Everything the daemon is begins here.
+ * @param arg_c number of command line arguments.
+ * @param arg_v character array delimited software arguments
+ */
+int main(int arg_c, char* arg_v[])
+{
+
+	// software return code, this will usually be EXIT_SUCCESS but can be modified by
+	// either the daemons return result, or the try/catch handler for this method.
+	int application_return_code = EXIT_SUCCESS;
+
+	using namespace inglenook;
+	using namespace inglenook::logging;
+
+	// number of threads, and number of messages to spawn.
+	const int NO_THREADS = 55; // more threads than queue space
+	const int NO_MESSAGES = 100; // lots of messages...
+
+	// thread collection
+	std::shared_ptr<boost::thread> threads[NO_THREADS];
+
+	// modify the default name space (if you do not do this the name space "inglenook.anonymous" will be used.
+	log_output->default_namespace("inglenook.logging.example");
+
+	// print out a small introduction block...
+	ilog.info()  << "This is an example of logging; and is for demonstration purposes.";
+	ilog << std::endl;
+	ilog	 << "This demo shows the expected use of the inglenook logging and output mechanism (inglenook::ilog)"                     << std::endl
+				 << "Its is also a test of thread safety and severe load conditions."                                                      << std::endl
+				 << "Launching "  << NO_THREADS << " threads / " << NO_MESSAGES    << "message per thread" 								   << std::endl
+				 << "================================================================================================"                     << std::endl
+				 << ""                                                                                                                     << lf::end;
+
+	// start all the threads
+	for(int i = 0; i < NO_THREADS; i++)
+	{
+		// create a new thread to spam logs with...
+		threads[i] = std::shared_ptr<boost::thread>(
+				new boost::thread(loud_thread, i, NO_MESSAGES));
+	}
+
+	// wait for all threads to re-join
+	for(int i = 0; i < NO_THREADS; i++)
+		threads[i]->join();
+
+	// indicate end of main to ensure all log messages are flushed..
+	ilog.info() << "The main() method has ended." << lf::end;
+
+}
